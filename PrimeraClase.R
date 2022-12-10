e904922509fd2caea4960f0dda8bfaa19f9d7b82
Michael De La Cruz <93782932+MaicolNoSkill@users.noreply.github.com>
2022-12-09 19:40:39 -0500

    Add files via upload

---
+++ PrimeraClase.R
@@ -0,0 +1,8 @@
+setwd('C:/Users/ACER/Desktop/CURSOS ONLINE/RMARKDOWN Y SHINY')
+getwd()
+
+# pacman::p_load(tinytex, rmarkdown, knitr) Instala y corre las librerías 
+
+library(tinytex)
+library(rmarkdown)
+library(knitr)
